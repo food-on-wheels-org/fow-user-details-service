dc3d9a4f88edcaab6e774a895062f75de2e22740
TejasSrivathsa <tejas.nmit@gmail.com>
2025-08-29 13:10:24 -0300

    Defect 001 - DB Mapping issues by Mapstruct

--- src/main/java/com/project/tejas/userdetails/mapper/UserDetailsMapper.java
+++ src/main/java/com/project/tejas/userdetails/mapper/UserDetailsMapper.java
@@ -3,11 +3,14 @@ package com.project.tejas.userdetails.mapper;
 import com.project.tejas.userdetails.dto.UserDetailsDTO;
 import com.project.tejas.userdetails.entity.UserDetails;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserDetailsMapper {
     UserDetailsMapper INSTANCE = Mappers.getMapper(UserDetailsMapper.class);
+
+    @Mapping(source = "userName", target = "userName")
     UserDetails mapUserDTOToUser(UserDetailsDTO userDetailsDTO);
     UserDetailsDTO mapUserToUserDetailsDTO(UserDetails userDetails);
 }
